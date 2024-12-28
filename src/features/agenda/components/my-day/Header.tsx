8e2756fc6ccb09783627505dbb7856f4ae074cd5
IraiDevPersonal <ignacio.arr01@gmail.com>
2024-12-28 15:11:21 -0300

    update: placeholder de busaccr mi dia

--- src/features/agenda/components/my-day/Header.tsx
+++ src/features/agenda/components/my-day/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
   return (
     <Box as="header" className="flex items-center w-full gap-4">
       <h1 className="mr-auto text-2xl font-bold">Mi Día</h1>
-      <Input.Search className="w-80" />
+      <Input.Search className="w-80" placeholder="Buscar paciente..." />
       <Input className="w-max" type="date" defaultValue={CURRENT_DATE} />
     </Box>
   );
