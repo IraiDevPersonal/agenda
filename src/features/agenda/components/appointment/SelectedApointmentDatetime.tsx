d092840de83b6069e7ac7e06b16d361db4078511
IraiDevPersonal <ignacio.arr01@gmail.com>
2025-01-13 21:48:23 -0300

    update: estilos de horario seleccionado

--- src/features/agenda/components/appointment/SelectedApointmentDatetime.tsx
+++ src/features/agenda/components/appointment/SelectedApointmentDatetime.tsx
@@ -16,13 +16,16 @@ export const SelectedApointmentDateTime: React.FC<Props> = ({ type }) => {
   return (
     <div
       className={cn(
-        "flex gap-2 rounded-xl p-3 *:font-bold italic w-full justify-center *:leading-none",
+        "flex gap-2 rounded-xl *:py-3 *:px-4 *:font-bold w-full justify-between *:leading-none overflow-hidden",
         HASH_COLORS[type]
       )}
     >
-      <span>Fecha: 11-01-2025</span>
-      <span>|</span>
-      <span>Horario: 13:00 - 13:45</span>
+      <div className="bg-white/60 h-full">Hora</div>
+      <div className="flex gap-3">
+        <span>Fecha: 11-01-2025</span>
+        <span>|</span>
+        <span>Horario: 13:00 - 13:45</span>
+      </div>
     </div>
   );
 };
