5d8bfec8e36a6e4606d46907d5b59b7aff7a1db0
IraiDevPersonal <ignacio.arr01@gmail.com>
2025-01-28 13:06:03 -0300

    add: integracion de calendario parcial

--- src/features/_core/components/ui/Button.tsx
+++ src/features/_core/components/ui/Button.tsx
@@ -3,7 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/config";
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-medium transition-colors duration-300 outline-offset-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-ring/70 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
   {
     variants: {
--- src/features/agenda/pages/AgendaPage.tsx
+++ src/features/agenda/pages/AgendaPage.tsx
@@ -1,16 +1,31 @@
+import Calendar from "@/features/_core/components/ui/Calendar";
 import Main from "@/features/_core/components/ui/Main";
+import { useState } from "react";
 import ViewProfessionalDataContext from "../context/ViewProfessionalDataContext";
-import AgendaHeader from "../components/agenda/AgendaHeader";
-import Appointments from "../components/appointment/Appoinments";
+// import AgendaHeader from "../components/agenda/AgendaHeader";
+// import Appointments from "../components/appointment/Appoinments";
 
 const AgendaPage = () => {
+  const [selected, setSelected] = useState<Date>();
   return (
     <ViewProfessionalDataContext showProfesionalData>
       <title>Agenda</title>
 
       <Main>
-        <AgendaHeader />
-        <Appointments />
+        <Calendar
+          mode="single"
+          selected={selected}
+          onSelect={setSelected}
+          renderDayAttachment={({ day }) => (
+            <>
+              <span>
+                {day.date.toDateString().includes("12") ? "Hora 1" : null}
+              </span>
+            </>
+          )}
+        />
+        {/* <AgendaHeader />
+        <Appointments /> */}
       </Main>
     </ViewProfessionalDataContext>
   );
