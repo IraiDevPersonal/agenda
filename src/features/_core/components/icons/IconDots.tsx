399a26ba78444f30dc53b4fb22fee9ad2fc8222b
IraiDevPersonal <ignacio.arr01@gmail.com>
2025-01-30 15:39:47 -0300

    add: nuevos iconos

---
+++ src/features/_core/components/icons/IconDots.tsx
@@ -0,0 +1,8 @@
+import type { IconProps } from "@/config";
+import { EllipsisIcon } from "lucide-react";
+
+const IconDots: React.FC<IconProps> = (props) => {
+  return <EllipsisIcon {...props} />;
+};
+
+export default IconDots;
---
+++ src/features/_core/components/icons/IconVerticalDots.tsx
@@ -0,0 +1,8 @@
+import type { IconProps } from "@/config";
+import { EllipsisVerticalIcon } from "lucide-react";
+
+const IconVerticalDots: React.FC<IconProps> = (props) => {
+  return <EllipsisVerticalIcon {...props} />;
+};
+
+export default IconVerticalDots;
